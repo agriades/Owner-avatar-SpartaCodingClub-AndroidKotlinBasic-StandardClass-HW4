9cb994110440c03b67354abd322473ff59e6b7d6
Sooyoung <agriades23@gmail.com>
2024-04-16 20:17:52 +0900

    [feat] RecyclerView에 들어갈 card item layout xml을 작성함.

--- app/src/main/java/com/standard/hw4/DetailActivity.kt
+++ app/src/main/java/com/standard/hw4/DetailActivity.kt
@@ -1,26 +1,16 @@
 package com.standard.hw4
 
-import android.R
 import android.os.Bundle
-import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.standard.hw4.databinding.ActivityMainBinding
 
+class DetailActivity : AppCompatActivity() {
 
-class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val dataList = mutableListOf(Cards.cardA, Cards.cardB, Cards.cardC)
-        val adapter = CardAdapter(dataList)
-        binding.cardRecyclerView.adapter = adapter
-        binding.cardRecyclerView.layoutManager = LinearLayoutManager(this)
     }
-
-
-
 }
