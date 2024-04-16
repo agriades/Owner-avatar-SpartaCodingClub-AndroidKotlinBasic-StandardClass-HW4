b0af738022aa5abb4e6cec301adb36851dc24b7b
Sooyoung <agriades23@gmail.com>
2024-04-16 21:13:11 +0900

    [feat] gradle에 id("kotlin-parcelize")를 추가하고 data class에 @Parcelize를 적용했는데, 자꾸 import가 되지 않았다. 안드로이드 스튜디오 재실행 후 [sync project with gradle files]로 해결했다.

--- app/src/main/java/com/standard/hw4/CardItem.kt
+++ app/src/main/java/com/standard/hw4/CardItem.kt
@@ -1,9 +1,13 @@
 package com.standard.hw4
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class CardItem(
     val iName: String,
     val iCardNumber: String,
     val iExpiryDate: String,
     val iBalance: String,
     val iBackground: Int
-)
+) : Parcelable
