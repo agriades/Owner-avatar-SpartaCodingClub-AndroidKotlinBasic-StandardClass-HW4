dec91d1395867b72c881fe98f4abe9f6034e12d3
Sooyoung <agriades23@gmail.com>
2024-04-16 21:16:40 +0900

    [feat] @Parcelize에 Null 허용

--- app/src/main/java/com/standard/hw4/CardItem.kt
+++ app/src/main/java/com/standard/hw4/CardItem.kt
@@ -5,9 +5,9 @@ import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class CardItem(
-    val iName: String,
-    val iCardNumber: String,
-    val iExpiryDate: String,
-    val iBalance: String,
-    val iBackground: Int
+    val iName: String?,
+    val iCardNumber: String?,
+    val iExpiryDate: String?,
+    val iBalance: String?,
+    val iBackground: Int?
 ) : Parcelable
