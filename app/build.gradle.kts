ff3c04954d4b4021b38740c63cd5d976490fd8e4
Sooyoung <agriades23@gmail.com>
2024-04-23 22:36:51 +0900

    [fix] dependencies에 androidx.activity 추가로 by viewModels()를 사용 가능하게 함.
    참고 블로그: https://mond-al.github.io/error-binding-notwork
    (몬드의 개발로그—Binding이 되지않을때 (by viewModels()에 에러))

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,6 +10,7 @@ android {
 
     buildFeatures{
         viewBinding = true
+        dataBinding = true
     }
 
     defaultConfig {
@@ -41,8 +42,12 @@ android {
 }
 
 dependencies {
+    val activity_version = "1.9.0"
+    implementation("androidx.activity:activity:$activity_version")
+    implementation("androidx.activity:activity-ktx:$activity_version")
     val lifecycle_version = "2.7.0"
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle_version")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
