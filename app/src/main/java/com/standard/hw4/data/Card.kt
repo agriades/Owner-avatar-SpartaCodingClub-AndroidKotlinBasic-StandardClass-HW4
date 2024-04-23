3d077406e1e8f94fbf23d7663937f296cbb36247
Sooyoung <agriades23@gmail.com>
2024-04-23 22:37:37 +0900

    [fix] 예시 블로그대로 ViewModel을 적용해 보기 위해 카드 한 장만 바로 Card.kt에 적용해 보기로 함.

--- app/src/main/java/com/standard/hw4/data/Card.kt
+++ app/src/main/java/com/standard/hw4/data/Card.kt
@@ -1,13 +1,27 @@
 package com.standard.hw4.data
 
 import android.os.Parcelable
+import com.standard.hw4.R
 import kotlinx.android.parcel.Parcelize
 
-@Parcelize
+//@Parcelize
 data class Card(
     val iName: String?,
     val iCardNumber: String?,
     val iExpiryDate: String?,
     val iBalance: Double?,
     val iBackground: Int?
-) : Parcelable
+) //: Parcelable
+
+class CardRepository {
+    fun getCardData(): Card {
+        // Fetch weather data from a remote server or local storage
+        return Card(
+            "DailyExpenses",
+            "2423 3581 9503 2412",
+            "07 / 24",
+            3100.30,
+            R.drawable.card_bg_deepblue
+        )
+    }
+}
