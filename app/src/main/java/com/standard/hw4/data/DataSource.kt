c1c5bb3bb1be3f68d6f0279e1e1b4c246df05da3
Sooyoung <agriades23@gmail.com>
2024-04-23 21:09:57 +0900

    [feat] DataSource.kt 의 내용을 flower 예시처럼 채워넣음. 아직도 왜 이렇게 써야 하는지 모르겠음.
    [모르겠는 부분] [question]
    - private var INSTANCE : DataSource ?= null 의 기능?
    - fun getDataSource() : DataSource 안의 코드는 무슨 뜻인지, 자기 이름(DataSource) 계속 먹일거면 이렇게 코드가 긴 이유가 뭔지?
    - fun getCardList() = cardList() 로 그대로 불러올거면 그냥 다른 곳에서 cardList()를 바로 불러오지 않는 이유가 있는지?
    - ViewModel을 사용하기 위해서 DataSource가 있는 것인지? 이전 코드에서 MVVM을 적용하지 않을 때에도 Singleton을 적용한 코드를 보여주었는데, 왜 사용하는지 이해가 안 되어서 적용을 못 했다. 이해하고 넘어가고 싶다.

--- app/src/main/java/com/standard/hw4/data/DataSource.kt
+++ app/src/main/java/com/standard/hw4/data/DataSource.kt
@@ -1,4 +1,21 @@
 package com.standard.hw4.data
 
 class DataSource {
+    companion object { //companion object가 즉 singleton이 된다.
+        private var INSTANCE : DataSource ?= null // [question] 무슨 의미지?
+        fun getDataSource() : DataSource { // [question] 왜 써야 하는지 모르겠지만 있으니까 우선 씀.
+            return synchronized(DataSource::class) {
+                val newInstance = INSTANCE ?: DataSource()
+                INSTANCE = newInstance
+                newInstance
+            }
+        }
+        fun getCardList() = cardList()
+    }
 }
+
+/* [question] Java나 phyton은 변수 선언 시 앞에 public이라고 선언하면 되는데,
+코틀린은 그게 안 되는 건가? 분명 public이 자동완성 되는데. 희한하네.
+[answer] 코틀린에서는 기본 접근 제한자가 public이다.
+[question]그러면 각자 다른 액티비티에서 왜 같은 이름의 변수가 안 불러와지는 거지?
+[answer] Java에서도 그냥 public이 아니라 public static 으로 선언해야 하나 봐. */
