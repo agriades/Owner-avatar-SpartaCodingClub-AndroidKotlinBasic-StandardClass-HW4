7d79accb1a60cb5a63f5fd805d4179e5e90959cb
Sooyoung <agriades23@gmail.com>
2024-04-23 21:15:30 +0900

    [feat] ViewModel 작성 중.
    - 정말 동적 데이터 처리 때문에 Singleton이 필요했나 보다. 그건 알겠는데...
    [question]
    - DataSource Instance 어쩌고 부분,왜 이렇게 말이 긴지는 여전히 이해 안 감.
    - import com.standard.hw4.data.DataSource 이 들어가 있는데도
     val cardsLiveData = dataSource.getCardList() 의 getCardList가 빨간 줄이 그인다.
     Unresolved reference: getCardList 라고 하는데, getCardList는 이미 class DataSource에 선언된 메소드다.

--- app/src/main/java/com/standard/hw4/presentation/CardViewModel.kt
+++ app/src/main/java/com/standard/hw4/presentation/CardViewModel.kt
@@ -2,6 +2,8 @@ package com.standard.hw4.presentation
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.standard.hw4.data.DataSource
 
-class CardViewModel: ViewModel() {
+class CardViewModel(val dataSource: DataSource): ViewModel() { //[question] 여기서 들어가는 DataSource가 무슨 값인지?
+    val cardsLiveData = dataSource.getCardList() //[answer]아하! 이거 하려고 이렇게 썼구나.
 }
