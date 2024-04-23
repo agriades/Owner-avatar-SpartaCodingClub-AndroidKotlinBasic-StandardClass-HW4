8a04bffbeedaba279a38ab665d0d0de105547f15
Sooyoung <agriades23@gmail.com>
2024-04-23 21:05:51 +0900

    [ref] 파일명 변경 - flower 예제를 따라하려는데 헷갈려서 비슷한 이름으로 바꿈. - 코드 구조에는 전혀 변화 없음.

--- app/src/main/java/com/standard/hw4/data/Card.kt
+++ app/src/main/java/com/standard/hw4/data/Card.kt
@@ -4,7 +4,7 @@ import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-data class CardItem(
+data class Card(
     val iName: String?,
     val iCardNumber: String?,
     val iExpiryDate: String?,
